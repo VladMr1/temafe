32c7433e44b30c12f74e95f456f5e082972dd493
Mercean Vlad <merceanv@gmail.com>
2021-11-20 16:32:07 +0200

    updatejs

---
+++ main.js
@@ -0,0 +1,42 @@
+ 
+ 
+    /*Jane Smith has $12,450 in her bank account.
+    $12,450 is a lot of money.
+    Jane Smith plans to buy a Toyota Yaris.
+    Jane Smith will use her $12,450 to buy a Toyota Yaris.
+    $12,450 is a lot of money for a Toyota Yaris.*/
+
+ 
+
+ let Jane Smith = nume;
+ var $12.450 = suma;
+ let Toyota Yaris = masina;
+ var money = bani;
+ let (is a lot of money) = propozitie1;
+ let in her bank account = propozitie2;
+ 
+ console.log(nume + ' has' + suma + propozitie2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
