69cbd8ee2cf1d7ec388b419405c150805f6767ce
Mercean Vlad <merceanv@gmail.com>
2021-11-21 17:46:40 +0200

    updatetema4

--- tema4/main.js
+++ tema4/main.js
@@ -1,21 +1,63 @@
  
  
-    /*Jane Smith has $12,450 in her bank account.
-    $12,450 is a lot of money.
-    Jane Smith plans to buy a Toyota Yaris.
-    Jane Smith will use her $12,450 to buy a Toyota Yaris.
-    $12,450 is a lot of money for a Toyota Yaris.*/
+ /*Jane Smith has $12,450 in her bank account.
+$12,450 is a lot of money.
+Jane Smith plans to buy a Toyota Yaris.
+Jane Smith will use her $12,450 to buy a Toyota Yaris.
+$12,450 is a lot of money for a Toyota Yaris.*/
 
  
 
- let herName = 'Jane Smith';
- var sum = '$12,450';
- let masina = 'Toyota Yaris';
- var money = 'bani';
- let propozitie1 = 'is a lot of money';
- let propozitie2 = ' in her bank account';
+        let herName = 'Jane Smith';
+        var sum = '$12,450';
+        let car = 'Toyota Yaris';
+        var money = 'bani';
+        let sentence1 = 'is a lot of money';
+        let sentence2 = ' in her bank account';
+        let plans = ' plans to buy a ';
+        let will = ' will use her ';
+        
+        console.log(herName + ' has ' + sum + sentence2);
+        console.log(sum + ' ' +  sentence1);
+        console.log(herName + plans + car );
+        console.log(herName + will + sum + ' to buy a ' + car);
+        console.log(sum + ' ' + sentence1 + ' for a ' + car);
  
- console.log(herName + ' has ' + sum + propozitie2);
+ 
+ 
+
+
+/*$12,450 is a lot of money.
+$12,450 is a lot of money for a Toyota Yaris.
+Jane Smith plans to buy a Toyota Yaris.
+Jane Smith has $12,450 in her bank account.
+Jane Smith will use her $12,450 to buy a Toyota Yaris.*/
+
+        console.log(sum + ' ' +  sentence1);
+        console.log(sum + ' ' + sentence1 + ' for a ' + car);
+        console.log(herName + plans + car );
+        console.log(herName + ' has ' + sum + sentence2);
+        console.log(herName + will + sum + ' to buy a ' + car);
+
+/* Jane Smith plans to buy a Toyota Yaris.
+$12,450 is a lot of money for a Toyota Yaris.
+Jane Smith will use her $12,450 to buy a Toyota Yaris.
+Jane Smith has $12,450 in her bank account.
+$12,450 is a lot of money.*/
+
+        console.log(herName + plans + car );
+        console.log(sum + ' ' + sentence1 + ' for a ' + car);
+        console.log(herName + will + sum + ' to buy a ' + car);
+        console.log(herName + ' has ' + sum + sentence2);
+        console.log(sum + ' ' +  sentence1);
+
+
+
+
+
+
+    
+
 
 
 
