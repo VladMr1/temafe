004f3cae36ada5086076f16a27f8fd61c490386b
Mercean Vlad <merceanv@gmail.com>
2021-11-30 16:42:26 +0200

    update tema 5

--- tema5/main.js
+++ tema5/main.js
@@ -18,7 +18,27 @@ closeButton.addEventListener('click', function() {
     popup.style.display = 'none';
 
     // moving the h1 content
-    var x = document.getElementById("h1").innerHTML;
-    document.getElementById("h11").innerHTML = x;
+    var a = document.getElementById("h1").innerHTML;
+    document.getElementById("h11").innerHTML = a;
+
+    //moving the p1 content
+    var b = document.getElementById("p1").innerHTML;
+    document.getElementById("p11").innerHTML = b;
+
+    //moving the p2 content
+    var c = document.getElementById("p2").innerHTML;
+    document.getElementById("p22").innerHTML = c;
+
+    //moving the divstrawberries content
+    var d = document.getElementsByClassName("s").innerHTML;
+    document.getElementsByClassName("s2").innerHTML = d;
+
+    // moving the ullist content
+     var e = document.getElementById("ullist").innerHTML;
+     document.getElementById("ullist2").innerHTML = e;
 });
+
+let list = document.getElementById("ullist").innerHTML;
+console.log(list);
+
 
