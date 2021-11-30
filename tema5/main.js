62eab652e62d801860aaa10b17e25ab8e8ddaa56
Mercean Vlad <merceanv@gmail.com>
2021-11-30 14:21:50 +0200

    tema5

---
+++ tema5/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+let closeButton = document.getElementById('headerClose');
+
+closeButton.addEventListener('click', function() {
+
+    let popup = document.getElementById('popup');
+    popup.style.display = 'none';
+});
+
+
+
+var x = document.getElementById("h1").innerHTML;
+
+document.getElementById("h11").innerHTML = x;
+
