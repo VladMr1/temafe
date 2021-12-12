78d8c0732f39e3be5b25b84cf20ac5dcba43c6d2
Tudor Goia <mindsetcoach360@gmail.com>
2021-12-12 21:40:55 +0200

    putting the seed for tema7

--- tema7/main.js
+++ tema7/main.js
@@ -1,5 +1,9 @@
 var x = prompt("Introduceti numarul dorit");
 
+let xAsNumber = parseInt(x);
+
+let parentArray = [];
+
 
 
 //let a = (Math.random()* 10);
@@ -7,12 +11,23 @@ var array;
 
 array=
 [
-    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+    [Math.floor(Math.random()* 10),[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
     [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
     [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
     [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
     [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
 ];
+
+for (let i = 0; i < xAsNumber; i = i + 1) {
+    // we need another for statement and also another array, not a parent array, but maybe a child array
+    parentArray.push([
+        Math.floor(Math.random()* 10),
+        Math.floor(Math.random()* 10),
+        Math.floor(Math.random()* 10),
+        Math.floor(Math.random()* 10),
+        Math.floor(Math.random()* 10)
+    ]);
+}
 
 let f = document.getElementsByClassName('randomnumbers').innerHTML = (array[5]);
 console.log((array[4]));
