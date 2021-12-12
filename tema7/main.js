de85a2b52ed28d0167bb93e2b9ee19180f2199e5
Tudor Goia <mindsetcoach360@gmail.com>
2021-12-12 22:01:57 +0200

    also exemplify createElement and outerHTML

--- tema7/main.js
+++ tema7/main.js
@@ -18,8 +18,16 @@ array=
     [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
 ];
 
+let parentElement = document.getElementById('parent');
+
 for (let i = 0; i < xAsNumber; i = i + 1) {
+    let lineDiv = document.createElement('div');
+    lineDiv.className = 'lineDiv';
+    parentElement.innerHTML = parentElement.innerHTML + lineDiv.outerHTML;
+
     // we need another for statement and also another array, not a parent array, but maybe a child array
+
+    
     parentArray.push([
         Math.floor(Math.random()* 10),
         Math.floor(Math.random()* 10),
