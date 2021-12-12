9226a69d77013071e93e4ad25c62a9d01dfc2939
Mercean Vlad <merceanv@gmail.com>
2021-12-12 20:29:38 +0200

    update7

---
+++ tema7/main.js
@@ -0,0 +1,21 @@
+var x = prompt("Introduceti numarul dorit");
+
+
+
+//let a = (Math.random()* 10);
+var array;
+
+array=
+[
+    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
+];
+
+let f = document.getElementsByClassName('randomnumbers').innerHTML = (array[5]);
+console.log((array[4]));
+
+
+
