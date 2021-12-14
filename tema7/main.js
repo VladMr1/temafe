dbbb2a18dd81d54926e5e7f71bf3c5e23a4ad944
Mercean Vlad <merceanv@gmail.com>
2021-12-14 18:40:37 +0200

    update tema7

--- tema7/main.js
+++ tema7/main.js
@@ -2,43 +2,28 @@ var x = prompt("Introduceti numarul dorit");
 
 let xAsNumber = parseInt(x);
 
-let parentArray = [];
+let childArray = [Math.floor(Math.random()* 10)];
+let parentArray = [childArray];
+console.log (parentArray);
 
 
-
-//let a = (Math.random()* 10);
-var array;
-
-array=
-[
-    [Math.floor(Math.random()* 10),[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
-    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
-    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
-    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
-    [[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)],[Math.floor(Math.random()* 10)]],
-];
-
-let parentElement = document.getElementById('parent');
+let parentElement = document.getElementById('parent').innerHTML = xAsNumber ;
 
 for (let i = 0; i < xAsNumber; i = i + 1) {
     let lineDiv = document.createElement('div');
     lineDiv.className = 'lineDiv';
     parentElement.innerHTML = parentElement.innerHTML + lineDiv.outerHTML;
+};
 
-    // we need another for statement and also another array, not a parent array, but maybe a child array
+if (xAsNumber % 2 ) {
+  document.getElementById('parent').innerHTML = xAsNumber;
+} else {(' ')};
+     //we need another for statement and also another array, not a parent array, but maybe a child array
 
     
-    parentArray.push([
-        Math.floor(Math.random()* 10),
-        Math.floor(Math.random()* 10),
-        Math.floor(Math.random()* 10),
-        Math.floor(Math.random()* 10),
-        Math.floor(Math.random()* 10)
-    ]);
-}
-
-let f = document.getElementsByClassName('randomnumbers').innerHTML = (array[5]);
-console.log((array[4]));
-
-
-
+   // parentArray.push([
+      // Math.floor(Math.random()* 10),
+       //Math.floor(Math.random()* 10),
+      // Math.floor(Math.random()* 10),
+       //Math.floor(Math.random()* 10),
+   // ])
