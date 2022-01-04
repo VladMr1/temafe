a89d7d53157c5bd83691bc25307a2120ebce4598
Mercean Vlad <merceanv@gmail.com>
2022-01-04 18:20:54 +0200

    update tema 9

--- tema9/main.js
+++ tema9/main.js
@@ -3,35 +3,23 @@ let launchesAxios = function(launchesAxiosFunction){
 let dataLaunches = launchesAxiosFunction.data;
 
 let renderOneCapsule = function(index) {
-let id = 'launches' + (index + 1);
-let capsulesId = dataLaunches[index].id;
-let capsulesFlightNumber = dataLaunches[index].flight_number;
-let shipsID = dataLaunches.ships;
-let ships =shipsID.lenght;
-document.getElementById('shipsTD').innerHTML = ships;
-document.getElementById(id).innerHTML = capsulesId;
-document.getElementById('launches1').innerHTML = capsulesFlightNumber;
+let idd = 'launches' + (index + 1);
+let capsulesId = dataLaunches[index].id + ': ' + dataLaunches[index].flight_number;
+//let shipsID = dataLaunches.ships;
+//let ships =shipsID.lenght;
 
-
+//document.getElementById('shipsTD').innerHTML = ships;
+document.getElementById(idd).innerHTML = capsulesId;
+//document.getElementById('launches1').innerHTML = capsulesFlightNumber;
 }
-
 
 for (let i = 0; i < 10; i = i + 1) {
     renderOneCapsule(i);
   }
 
 };
-  
-
-
-
-
-
-
 
 axios.get('https://api.spacexdata.com/v4/launches').then(launchesAxios);
-
-
 
 
 let d = new Date();
